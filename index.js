a97544cfeee6edf55bd398708c47823097a4dd28
ninodev92 <daren2track@gmail.com>
2025-10-17 11:31:56 +0100

    added resend

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
 const dotenv = require("dotenv");
 dotenv.config({ debug: true });
 const port = process.env.PORT ?? 5000;
@@ -27,17 +28,31 @@ app.post("/api/contact", (req, res) => {
   });
 
   async function main() {
-    const transporter = nodemailer.createTransport({
-      host: "",
-      port: "",
-      service: "gmail",
-      auth: {
-        user: user,
-        pass: pass,
-      },
-    });
-    const info = await transporter.sendMail({
-      from: "Backend <nnejirichard992@gmail.com>",
+    // const transporter = nodemailer.createTransport({
+    //   host: "",
+    //   port: "",
+    //   service: "gmail",
+    //   auth: {
+    //     user: user,
+    //     pass: pass,
+    //   },
+    // });
+    // const info = await transporter.sendMail({
+    //   from: "Backend <nnejirichard992@gmail.com>",
+    //   to: "nnejirichard@yahoo.com",
+    //   subject: "Contact Form Details",
+    //   html: `<p>Name: ${firstname} ${lastname}</p>
+    //   <p>Email: ${email}</p>
+    //   <p>Phone: ${number}</p>
+    //   <p>Area of Interest: ${subjectselect}</p>
+    //   <p>Message: ${message}</p>
+    //   `,
+    // });
+    // console.log("Message sent", info.messageId);
+    const resend = new Resend("re_EACEjN7J_AAZW9eio6E2m3sA1AdgdQb74");
+
+    resend.emails.send({
+      from: "Onboarding <onboarding@resend.dev>",
       to: "nnejirichard@yahoo.com",
       subject: "Contact Form Details",
       html: `<p>Name: ${firstname} ${lastname}</p>
@@ -47,8 +62,6 @@ app.post("/api/contact", (req, res) => {
       <p>Message: ${message}</p>
       `,
     });
-
-    console.log("Message sent", info.messageId);
   }
   main().catch((error) => console.log(error));
   res.send("message sent");
